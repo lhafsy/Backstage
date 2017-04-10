6e9ac6a3e3eccc19db2a6badd39a56e8680cb48e
lha <lha@qq.com>
2017-04-10 09:05:57 +0800

    a

--- src/component/delete.jsx
+++ src/component/delete.jsx
@@ -1,20 +1,53 @@
 
-module.exports = AppComponentCopy;
 
+var $ = require("jquery");
 var React = require('react');
 var ReactRouter = require("react-router");
 var { Router, Route, hashHistory, Link } = ReactRouter;
 
 var AppComponentCopy = React.createClass({
-  render() {
-    // return <div>电影</div>;
-    // 路由嵌套
-    return (
-       <div>
-       <h1>删除</h1>
-       </div>
-    );
+  getDefaultProps:function() {
+    return {
+      columns: [],
+      columns1:[]
+    }
+  },
+  getInitialState: function(){
+    return {value:[]}
+  },
+  componentDidMount:function(){
+    var self =this
+    $.ajax({
+      url:'http://localhost:888/lha',
+      type: "GET",
+      async:false,
+      success: function(msg){
+        self.setState({value:msg});
+        // console.log(this);
+        // console.log(self);
+        console.log(1)
+      }
+    })
+  },
+  render:function(){
+   console.log(2)
+   for(var key in this.state.value[0]){
+    this.props.columns.push(key);
   }
+  console.log(this.props.columns)
+  console.log(this.state.value[0])
+
+  return (
+   <thead>
+   <tr>
+   {
+    this.props.columns.map(function(elem, index) {
+      return <th name={elem} key={elem}>{elem}</th>;})
+  }
+  </tr>
+  </thead>
+  );
+}
 });
 
 module.exports = AppComponentCopy;
